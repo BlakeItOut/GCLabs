f5b971ff7bd0b5facad6870679879cbc611fd496
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-09 13:21:11 -0400

    Added tryparse, get your name feature, and continuous use.

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -10,26 +10,41 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            int entry = 1;
-            while (entry >= 1 && entry <= 100)
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            while (true)
             {
-                Console.Write("Enter an integer between 1 and 100:");
-                entry = int.Parse(Console.ReadLine());
+                bool res = false;
+                int entry = 0;
+                Console.WriteLine("");
+                while (entry < 1 || entry > 100 || !res)
+                {
+                    Console.Write("Enter an integer between 1 and 100: ");
+                    res = int.TryParse(Console.ReadLine(), out entry);
+                }
+                Console.Write("For you, " + name + ": ");
+                if (entry % 2 == 1)
+                {
+                    Console.WriteLine(entry + " Odd");
+                }
+                else if (entry <= 24)
+                {
+                    Console.WriteLine("Even and less than 25.");
+                }
+                else if (entry <= 60)
+                {
+                    Console.WriteLine("Even.");
+                }
+                else
+                {
+                    Console.WriteLine(entry + " Even.");
+                }
+                Console.Write("Would you like to continue?(y/n) ");
+                if(Char.ToLower(Console.ReadKey().KeyChar) == 'n')
+                {
+                    break;
+                }
             }
-            if (entry % 2 == 1)
-            {
-                Console.WriteLine(entry + " Odd");
-            } else if (entry <= 24)
-            {
-                Console.WriteLine("Even and less than 25.");
-            } else if (entry <= 60)
-            {
-                Console.WriteLine("Even.");
-            } else
-            {
-                Console.WriteLine(entry + " Even.");
-            }
-            Console.ReadKey();
         }
     }
 }
