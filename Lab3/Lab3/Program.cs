963e816f7e83c7ab45a927bd43780865da64a822
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-09 11:23:36 -0400

    Added lab3 initial.

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer between 1 and 100:");
+            int entry = int.Parse(Console.ReadLine());
+            if (entry % 2 == 1)
+            {
+                Console.WriteLine(entry + " Odd");
+            } else if (entry <= 24)
+            {
+                Console.WriteLine("Even and less than 25.");
+            } else if (entry <= 60)
+            {
+                Console.WriteLine("Even.");
+            } else
+            {
+                Console.WriteLine(entry + " Even.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
