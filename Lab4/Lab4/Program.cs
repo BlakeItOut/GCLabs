7bf6b49697a4434f71ecaa08202e9cb84ddc1ef7
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-09 17:09:36 -0400

    differentiated super with normal mode for lab 4

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -12,39 +12,62 @@ namespace Lab4
         {
             Console.WriteLine("Learn your squares and cubes!");
             Console.WriteLine("");
-            while (true)
+            Console.Write("What mode would you like to enter? (n=normal, s=super)");
+            if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
             {
-                Console.Write("Enter an integer or enter q to quit: ");
-                string entry = Console.ReadLine();
-                if (entry.ToLower() == "q")
+                while (true)
                 {
-                    break;
+                    Console.WriteLine("");
+                    Console.WriteLine("Learn your squares and cubes!");
+                    Console.WriteLine("");
+                    Console.Write("Enter an integer: ");
+                    int upTo = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+                    Console.WriteLine("Number     Squared    Cubed");
+                    Console.WriteLine("======     =======    =====");
+                    for (int i = 1; i <= upTo; i++)
+                    {
+                        Console.WriteLine("{0,-11:0}{1,-11:0}{2,-11:0}", i, i*i, i*i*i);
+                    }
+                    Console.Write("Continue? (y/n):");
+                    if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
+                    {
+                        break;
+                    } 
+                    Console.WriteLine("");
                 }
-                int upTo = int.Parse(entry);
+            } else
+            {
                 Console.WriteLine("");
-                for (int j = 1; j <= upTo; j++)
+                while (true)
                 {
-                    Console.Write("{0,-11:0}", "^"+j);
-                }
-                Console.WriteLine("");
-                for (int j = 1; j <= upTo; j++)
-                {
-                    Console.Write("{0,-11:0}", "=====");
-                }
-                Console.WriteLine("");
-                for (int i = 1; i <= upTo; i++)
-                {
-                    for (int j = 1; j <= upTo; j++) {
-                        Console.Write("{0,-11:0}", Math.Pow(i, j));
+                    Console.Write("Enter an integer or enter q to quit: ");
+                    string entry = Console.ReadLine();
+                    if (entry.ToLower() == "q")
+                    {
+                        break;
+                    }
+                    int upTo = int.Parse(entry);
+                    Console.WriteLine("");
+                    for (int j = 1; j <= upTo; j++)
+                    {
+                        Console.Write("{0,-11:0}", "^" + j);
+                    }
+                    Console.WriteLine("");
+                    for (int j = 1; j <= upTo; j++)
+                    {
+                        Console.Write("{0,-11:0}", "=====");
+                    }
+                    Console.WriteLine("");
+                    for (int i = 1; i <= upTo; i++)
+                    {
+                        for (int j = 1; j <= upTo; j++) {
+                            Console.Write("{0,-11:0}", Math.Pow(i, j));
+                        }
+                        Console.WriteLine("");
                     }
                     Console.WriteLine("");
                 }
-                //Console.Write("Continue? (y/n):");
-                //if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
-                //{
-                //    break;
-                //}
-                Console.WriteLine("");
             }
         }
     }
