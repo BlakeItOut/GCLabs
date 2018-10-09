e1868dd89550ef67f376ffc9a13a93425dad135a
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-09 11:08:38 -0400

    Initial Commit for lab2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+            Console.WriteLine("");
+            while (true)
+            {
+                double length = errorHandling("Length");
+                double width = errorHandling("Width");
+                Console.WriteLine("Area:" + (width * length).ToString());
+                Console.WriteLine("Perimeter:" + ((width + length) * 2).ToString());
+                Console.WriteLine("");
+                Console.Write("Continue?(y/n):");
+                char response = Console.ReadKey().KeyChar;
+                if (response == 'n')
+                {
+                    break;
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        static double errorHandling(string entry)
+        {
+            double doubledEntry;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter " + entry + ":");
+                    doubledEntry = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    continue;
+                }
+            }
+            return doubledEntry;
+
+        }
+    }
+}
