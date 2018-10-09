f4a5c37a0d7a1950b0b823b16a1d0b018de68e17
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-09 16:09:51 -0400

    Update to q for quit and run across and down.

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -10,26 +10,40 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Learn your squares and cubes!");
+            Console.WriteLine("");
             while (true)
             {
-                Console.WriteLine("Learn your squares and cubes!");
-                Console.WriteLine("");
-                Console.Write("Enter an integer: ");
-                int upTo = int.Parse(Console.ReadLine());
-                Console.WriteLine("");
-                Console.WriteLine("Number     Squared    Cubed");
-                Console.WriteLine("======     =======    =====");
-                for (int i = 1; i <= upTo; i++)
-                {
-                    int square = i * i;
-                    int cube = square * i;
-                    Console.WriteLine("{0,-11:0}{1,-11:0}{2,-11:0}", i, square, cube);
-                }
-                Console.Write("Continue? (y/n):");
-                if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
+                Console.Write("Enter an integer or enter q to quit: ");
+                string entry = Console.ReadLine();
+                if (entry.ToLower() == "q")
                 {
                     break;
                 }
+                int upTo = int.Parse(entry);
+                Console.WriteLine("");
+                for (int j = 1; j <= upTo; j++)
+                {
+                    Console.Write("{0,-11:0}", "^"+j);
+                }
+                Console.WriteLine("");
+                for (int j = 1; j <= upTo; j++)
+                {
+                    Console.Write("{0,-11:0}", "=====");
+                }
+                Console.WriteLine("");
+                for (int i = 1; i <= upTo; i++)
+                {
+                    for (int j = 1; j <= upTo; j++) {
+                        Console.Write("{0,-11:0}", Math.Pow(i, j));
+                    }
+                    Console.WriteLine("");
+                }
+                //Console.Write("Continue? (y/n):");
+                //if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
+                //{
+                //    break;
+                //}
                 Console.WriteLine("");
             }
         }
