8b03037ab0790112f7d2e912b37a954fa3131651
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-10 09:00:46 -0400

    Allow switching between modes.

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -10,63 +10,75 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Learn your squares and cubes!");
+            Console.WriteLine("Learn your squares, cubes, and more!");
             Console.WriteLine("");
-            Console.Write("What mode would you like to enter? (n=normal, s=super)");
-            if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
+            while (true)
             {
-                while (true)
+                Console.Write("What mode would you like to enter? (n=normal, s=super, q=quit)");
+                Char mode = Char.ToLower(Console.ReadKey().KeyChar);
+                if (mode == 'n')
                 {
                     Console.WriteLine("");
-                    Console.WriteLine("Learn your squares and cubes!");
-                    Console.WriteLine("");
-                    Console.Write("Enter an integer: ");
-                    int upTo = int.Parse(Console.ReadLine());
-                    Console.WriteLine("");
-                    Console.WriteLine("Number     Squared    Cubed");
-                    Console.WriteLine("======     =======    =====");
-                    for (int i = 1; i <= upTo; i++)
+                    while (true)
                     {
-                        Console.WriteLine("{0,-11:0}{1,-11:0}{2,-11:0}", i, i*i, i*i*i);
-                    }
-                    Console.Write("Continue? (y/n):");
-                    if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
-                    {
-                        break;
-                    } 
-                    Console.WriteLine("");
-                }
-            } else
-            {
-                Console.WriteLine("");
-                while (true)
-                {
-                    Console.Write("Enter an integer or enter q to quit: ");
-                    string entry = Console.ReadLine();
-                    if (entry.ToLower() == "q")
-                    {
-                        break;
-                    }
-                    int upTo = int.Parse(entry);
-                    Console.WriteLine("");
-                    for (int j = 1; j <= upTo; j++)
-                    {
-                        Console.Write("{0,-11:0}", "^" + j);
-                    }
-                    Console.WriteLine("");
-                    for (int j = 1; j <= upTo; j++)
-                    {
-                        Console.Write("{0,-11:0}", "=====");
-                    }
-                    Console.WriteLine("");
-                    for (int i = 1; i <= upTo; i++)
-                    {
-                        for (int j = 1; j <= upTo; j++) {
-                            Console.Write("{0,-11:0}", Math.Pow(i, j));
+                        Console.Write("Enter an integer: ");
+                        int upTo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
+                        Console.WriteLine("Number     Squared    Cubed");
+                        Console.WriteLine("======     =======    =====");
+                        for (int i = 1; i <= upTo; i++)
+                        {
+                            Console.WriteLine("{0,-11:0}{1,-11:0}{2,-11:0}", i, i * i, i * i * i);
+                        }
+                        Console.Write("Continue? (y/n):");
+                        if (Char.ToLower(Console.ReadKey().KeyChar) == 'n')
+                        {
+                            Console.WriteLine("");
+                            break;
                         }
                         Console.WriteLine("");
                     }
+                }
+                else if (mode == 's')
+                {
                     Console.WriteLine("");
+                    while (true)
+                    {
+                        Console.Write("Enter an integer or enter m to return to menu: ");
+                        string entry = Console.ReadLine();
+                        if (entry.ToLower() == "m")
+                        {
+                            break;
+                        }
+                        int upTo = int.Parse(entry);
+                        Console.WriteLine("");
+                        for (int j = 1; j <= upTo; j++)
+                        {
+                            Console.Write("{0,-11:0}", "^" + j);
+                        }
+                        Console.WriteLine("");
+                        for (int j = 1; j <= upTo; j++)
+                        {
+                            Console.Write("{0,-11:0}", "=====");
+                        }
+                        Console.WriteLine("");
+                        for (int i = 1; i <= upTo; i++)
+                        {
+                            for (int j = 1; j <= upTo; j++)
+                            {
+                                Console.Write("{0,-11:0}", Math.Pow(i, j));
+                            }
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("");
+                    }
+                } else if (mode == 'q')
+                {
+                    break;
+                } else
+                {
+                    Console.WriteLine();
+                    continue;
                 }
             }
         }
