aaa923c12872d5ad91ec885b491db58c75f41a2f
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-10-11 13:27:07 -0400

    Added speech part for after regular part.

--- Capstone1/Capstone1/Program.cs
+++ Capstone1/Capstone1/Program.cs
@@ -5,12 +5,108 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace Capstone1
 {
     class Program
     {
         static void Main()
+        {
+                Console.WriteLine("Welcome to the Pig Latin Translator!");
+            while (true)
+            {
+                Console.WriteLine("");
+                startText();
+                Console.WriteLine("");
+                Console.Write("Translate another line? (y/n) ");
+                if (Char.ToLower(Console.ReadKey().KeyChar) == 'n') break;
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Now the real fun begins");
+            startSpeech();
+        }
+
+        static void startText()
+        {
+            string userInput = null;
+            while (string.IsNullOrEmpty(userInput))
+            {
+                Console.Write("Enter a word to be translated: ");
+                userInput = Console.ReadLine();
+            }
+            Console.WriteLine("");
+            Console.WriteLine(translate(userInput));
+        }
+
+        static string translate(string input)
+        {
+            //initialize to use ToTitleCase
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            string [] inputArray = input.Split(' ');
+
+            string [] output = new string [inputArray.Length];
+            
+            for (int j = 0; j < inputArray.Length; j++)
+            {
+                string word = inputArray[j];
+
+                //check the string only contains alphabet and punctuations else leave it the same
+                if (Regex.IsMatch(word, @"^[A-Za-z\',;.!?]+$"))
+                {
+                    //checks if the word is in title case
+                    bool titleCase = Regex.IsMatch(word, @"([A-Z][^A-Z])\w+");
+                    char punctuation = ' ';
+                    //check if there is punctuation, if so, chop it off and store for later
+                    if (Regex.IsMatch(word[word.Length - 1].ToString(), @"[,;.!?]"))
+                    {
+                        punctuation = word[word.Length - 1];
+                        word = word.Substring(0, word.Length - 1);
+                    }
+                    //if starts with vowel add way to end else move all consonants before first vowel to the end then add ay to the end of the word.
+                    if (Regex.IsMatch(word[0].ToString(), @"[aeiouAEIOU]"))
+                    {
+                        word += "way";
+                    }
+                    else
+                    {
+                        int firstVowel = 0;
+                        for (int i = 0; i < word.Length; i++)
+                        {
+                            if (Regex.IsMatch(word[i].ToString(), @"[aeiouAEIOU]"))
+                            {
+                                firstVowel = i;
+                                break;
+                            }
+                        }
+                        word = word.Substring(firstVowel) + word.Substring(0, firstVowel) + "ay";
+                    }
+                    //if the input is title case make sure the output is too
+                    if (titleCase)
+                    {
+                        word = textInfo.ToTitleCase(word);
+                    }
+                    //if there was punchtuation put it back
+                    if (punctuation != ' ')
+                    {
+                        word += punctuation;
+                    }
+                    //Make all upper case if the first two letter are upper case (indicative of all upper case)
+                    if (Regex.IsMatch(word,@"([A-Z][A-Z])\w+"))
+                    {
+                        word = word.ToUpper();
+                    }
+                }
+                output[j] = word;
+            }
+            return String.Join(" ",output);
+        }
+
+        static void startSpeech()
         {
             // Create a SpeechRecognitionEngine object for the default recognizer in the en-US locale.
             using (
@@ -23,7 +119,7 @@ namespace Capstone1
 
                 // Add a handler for the speech recognized event.
                 recognizer.SpeechRecognized +=
-                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+                  new EventHandler<SpeechRecognizedEventArgs>(displayResults);
 
                 // Configure the input to the speech recognizer.
                 recognizer.SetInputToDefaultAudioDevice();
@@ -40,9 +136,16 @@ namespace Capstone1
         }
 
         // Handle the SpeechRecognized event.
-        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        static void displayResults(object sender, SpeechRecognizedEventArgs e)
         {
             Console.WriteLine("Recognized text: " + e.Result.Text);
+            Console.WriteLine("Translated text: " + translate(e.Result.Text));
+            saySomething(translate(e.Result.Text));
+            Console.WriteLine("");
+        }
+
+        static void saySomething(string something)
+        {
             // Initialize a new instance of the SpeechSynthesizer.  
             SpeechSynthesizer synth = new SpeechSynthesizer();
 
@@ -50,7 +153,7 @@ namespace Capstone1
             synth.SetOutputToDefaultAudioDevice();
 
             // Speak a string.  
-            synth.Speak(e.Result.Text);
+            synth.Speak(something);
         }
     }
 }
