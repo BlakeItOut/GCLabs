e1aab7e566b6207e99685a75107df3f041cf6fba
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-01 15:09:42 -0400

    Added method to grab all the primes up to 100000 so after it would run faster but it does run slow at the beginning to grab these once.

--- Lab17/Lab17/Program.cs
+++ Lab17/Lab17/Program.cs
@@ -8,14 +8,57 @@ namespace Lab17
 {
     public class Program
     {
+        static int[] primes = GetUpTo100000ThPrime();
         public static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's locate some primes!\n\nThis application will find you any prime, in order, from the first prime number (2) on.");
+            while (true)
+            {
+                int nth = int.Parse(Validator.promptUser("Which sequence number prime are you looking for? (between 1 and 100000, inclusive) ", (num => int.TryParse(num, out nth) && nth > 0 && nth <= 100000)));
+                Console.WriteLine($"\nThe sequence number {nth} prime is {LocatePrime(nth)}.\n");
+                if (!Validator.promptYN("Do you want to find another prime number? (y/n) "))
+                {
+                    break;
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Goodbye");
             Console.ReadKey();
+            
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        public static int LocatePrime(int nth)
+        {
+            return primes[nth-1];
+        }
+
+        public static bool CheckPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            if (number % 2 == 0) return false;     
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static int[] GetUpTo100000ThPrime()
+        {
+            int [] primes = new int [100000];
+            int counter = 0;
+            for(int i = 0; i <= 1299709; i++)
+            {
+                if (CheckPrime(i))
+                {
+                    primes[counter++] = i;
+                }
+            }
+            return primes;
         }
     }
 }
--- Lab17/Lab17Tests/ProgramTests.cs
+++ Lab17/Lab17Tests/ProgramTests.cs
@@ -12,12 +12,54 @@ namespace Lab17.Tests
     public class ProgramTests
     {
         [TestMethod()]
-        public void LocatePrime_ReturnTheNthPrime_True()
+        public void LocatePrime_ReturnTheSmallNthPrime_True()
         {
             var target = 6;
             var expected = 13;
-            var actual = LocatePrime(target);
-            Assert.Fail();
+            var actual = Program.LocatePrime(target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LocatePrime_ReturnTheBigNthPrime_True()
+        {
+            var target = 1000;
+            var expected = 7919;
+            var actual = Program.LocatePrime(target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LocatePrime_ReturnTheWrongNthPrime_False()
+        {
+            var target = 5;
+            var notExpected = 13;
+            var actual = Program.LocatePrime(target);
+            Assert.AreNotEqual(notExpected, actual);
+        }
+
+        [TestMethod()]
+        public void CheckPrime_ReturnBoolIfPrime_True()
+        {
+            var target = 7;
+
+            Assert.IsTrue(Program.CheckPrime(target));
+        }
+
+        [TestMethod()]
+        public void GetUpTo100000ThPrime_ReturnArrayMin_True()
+        {
+            var expected = 2;
+            var actual = Program.GetUpTo100000ThPrime().First();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetUpTo100000ThPrime_ReturnArrayMax_True()
+        {
+            var expected = 1299709;
+            var actual = Program.GetUpTo100000ThPrime().Last();
+            Assert.AreEqual(expected, actual);
         }
     }
 }
