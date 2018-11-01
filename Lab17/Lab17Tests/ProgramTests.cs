a098efd576fb3697fea062ffaebe9e0af4bf8950
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-01 13:24:57 -0400

    Starting lab 17

---
+++ Lab17/Lab17Tests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab17;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void LocatePrime_ReturnTheNthPrime_True()
+        {
+            var target = 6;
+            var expected = 13;
+            var actual = LocatePrime(target);
+            Assert.Fail();
+        }
+    }
+}
