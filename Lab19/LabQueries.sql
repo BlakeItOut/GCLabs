2bc4670d92141e1ed359254e158177345df32a66
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-12-04 11:35:05 -0500

    added bonus 20 to lab 19

--- Lab19/LabQueries.sql
+++ Lab19/LabQueries.sql
@@ -1,3 +1,4 @@
+--Lab 19
 SELECT *
 FROM Customers;
 
@@ -22,6 +23,9 @@ WHERE ShipRegion IS NOT NULL;
 SELECT *
 FROM Customers
 ORDER BY Country, City;
+
+--DELETE FROM Customers
+--WHERE CustomerID = 'ABCDE';
 
 INSERT INTO Customers
 VALUES ('ABCDE', 'Whatever Co', 'Jim Bob', 'WhatsIt', '123 Anywhere', 'Anywhere', 'QS', '1234', 'United States', '(1) 123-1234', '(1) 123-1234');
@@ -66,4 +70,84 @@ WHERE ReportsTo =
 (SELECT EmployeeID
 FROM Employees
 WHERE FirstName = 'Andrew');
+
+--Bonus 20
+SELECT *
+FROM Customers;
+
+SELECT *
+FROM Customers
+WHERE City IN ('London', 'Paris');
+
+SELECT DISTINCT City
+FROM Customers;
+
+SELECT FirstName
+FROM Employees
+ORDER BY FirstName;
+
+SELECT FORMAT (avg(Salary), 'C', 'en-us') as AvgSalary
+FROM Salaries;
+
+SELECT TOP 1 FORMAT (Salary, 'C', 'en-us') as AvgSalary, Employees.EmployeeID, LastName, FirstName
+FROM Salaries
+LEFT JOIN Employees ON Salaries.EmployeeID = Employees.EmployeeID
+ORDER BY Salary DESC;
+
+SELECT *
+FROM Employees
+WHERE Notes LIKE '%BA%';
+
+SELECT *
+FROM [Summary of Sales by Year];
+
+SELECT *
+FROM Employees
+WHERE HireDate > Convert(datetime, '1994-1-1');
+
+SELECT FirstName, DATEDIFF(year, HireDate, GETDATE()) as TimeWorked
+FROM Employees;
+
+SELECT *
+FROM Products
+ORDER BY (UnitsInStock+UnitsOnOrder);
+
+SELECT *
+FROM Products
+ORDER BY (UnitsInStock+UnitsOnOrder) DESC;
+
+SELECT *
+FROM Products
+WHERE (UnitsInStock + UnitsOnOrder) < 6;
+
+SELECT *
+FROM Products
+WHERE Discontinued = 1;
+
+SELECT *
+FROM Products
+WHERE ProductName LIKE '%tofu%';
+
+SELECT TOP 1 *
+FROM Products
+ORDER BY UnitPrice DESC;
+
+SELECT *
+FROM Employees
+WHERE HireDate > Convert(datetime, '1993-1-1');
+
+SELECT *
+FROM Employees
+WHERE TitleOfCourtesy IN ('Ms.', 'Mrs.');
+
+SELECT *
+FROM Employees
+WHERE HomePhone LIKE '(206%';
+
+SELECT      COLUMN_NAME AS 'ColumnName'
+            ,TABLE_NAME AS  'TableName'
+FROM        INFORMATION_SCHEMA.COLUMNS
+--WHERE       COLUMN_NAME LIKE '%pay%'
+ORDER BY    TableName
+            ,ColumnName;
 
